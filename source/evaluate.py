b33bf62ce3a993a298e661cc25f4eb0609745380
Casey Casalnuovo <caseycasal@gmail.com>
2017-05-09 21:35:57 -0700

    Fixing indents.

--- source/evaluate.py
+++ source/evaluate.py
@@ -14,57 +14,57 @@ from model import Model
 from six import text_type
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('save_dir', type=str, default='save',
-					   help='model directory to store checkpointed models')
-	parser.add_argument('source', type=str, default='code.c',
-					   help='source file to evaluate')
-	parser.add_argument('--pre_tokenized', type=str, default='false',
-					   help='boolean indicating if the source file is already tokenized')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('save_dir', type=str, default='save',
+                       help='model directory to store checkpointed models')
+    parser.add_argument('source', type=str, default='code.c',
+                       help='source file to evaluate')
+    parser.add_argument('--pre_tokenized', type=str, default='false',
+                       help='boolean indicating if the source file is already tokenized')
 
-	args = parser.parse_args()
-	evaluate(args)
+    args = parser.parse_args()
+    evaluate(args)
 
 def evaluate(args):
-	pre_tokenized = str2bool(args.pre_tokenized)
-	with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
-		(saved_args, reverse_input) = cPickle.load(f)
-	with open(os.path.join(args.save_dir, 'token_vocab.pkl'), 'rb') as f:
-		tokens, vocab = cPickle.load(f)
-	model = Model(saved_args, reverse_input, True)
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		saver = tf.train.Saver(tf.global_variables())
-		ckpt = tf.train.get_checkpoint_state(args.save_dir)
-		if ckpt and ckpt.model_checkpoint_path:
-			saver.restore(sess, ckpt.model_checkpoint_path)
-			if pre_tokenized:
-				with open(args.source, 'r') as f:
-					token_list = f.read().split()
-			else:
-				token_list = get_tokens(args.source)
-				
-			token_list = convert_to_vocab_tokens(vocab, token_list, model.start_token,
-				model.end_token, model.unk_token)
-			probs = model.evaluate(sess, tokens, vocab, token_list)
+    pre_tokenized = str2bool(args.pre_tokenized)
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+        (saved_args, reverse_input) = cPickle.load(f)
+    with open(os.path.join(args.save_dir, 'token_vocab.pkl'), 'rb') as f:
+        tokens, vocab = cPickle.load(f)
+    model = Model(saved_args, reverse_input, True)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver(tf.global_variables())
+        ckpt = tf.train.get_checkpoint_state(args.save_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            if pre_tokenized:
+                with open(args.source, 'r') as f:
+                    token_list = f.read().split()
+            else:
+                token_list = get_tokens(args.source)
+                
+            token_list = convert_to_vocab_tokens(vocab, token_list, model.start_token,
+                model.end_token, model.unk_token)
+            probs = model.evaluate(sess, tokens, vocab, token_list)
 
 def str2bool(s):
-	return s.lower() in ('t', 'true', '1', 'yes')
+    return s.lower() in ('t', 'true', '1', 'yes')
 
 def get_tokens(source):
-	tokenized_code, _ = simplePyLex.tokenize_file(source, 3)
-	return tokenized_code.split()
+    tokenized_code, _ = simplePyLex.tokenize_file(source, 3)
+    return tokenized_code.split()
 
 def convert_to_vocab_tokens(vocab, token_list, start_token, end_token, unk_token):
-	res = [start_token]
-	for token in token_list:
-		if token in vocab:
-			res.append(token)
-		else:
-			res.append(unk_token)
+    res = [start_token]
+    for token in token_list:
+        if token in vocab:
+            res.append(token)
+        else:
+            res.append(unk_token)
 
-	res.append(end_token)
-	return res
+    res.append(end_token)
+    return res
 
 if __name__ == '__main__':
-	main()
+    main()
--- source/sample.py
+++ source/sample.py
@@ -13,30 +13,30 @@ from model import Model
 from six import text_type
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('save_dir', type=str, default='save',
-					   help='model directory to store checkpointed models')
-	parser.add_argument('--sample', type=int, default=1,
-					   help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
-	parser.add_argument('--max_tokens', type=int, default=500,
-					   help='maximum number of tokens to generate')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('save_dir', type=str, default='save',
+                       help='model directory to store checkpointed models')
+    parser.add_argument('--sample', type=int, default=1,
+                       help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
+    parser.add_argument('--max_tokens', type=int, default=500,
+                       help='maximum number of tokens to generate')
 
-	args = parser.parse_args()
-	sample(args)
+    args = parser.parse_args()
+    sample(args)
 
 def sample(args):
-	with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
-		(saved_args, reverse_input) = cPickle.load(f)
-	with open(os.path.join(args.save_dir, 'token_vocab.pkl'), 'rb') as f:
-		tokens, vocab = cPickle.load(f)
-	model = Model(saved_args, reverse_input, True)
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		saver = tf.train.Saver(tf.global_variables())
-		ckpt = tf.train.get_checkpoint_state(args.save_dir)
-		if ckpt and ckpt.model_checkpoint_path:
-			saver.restore(sess, ckpt.model_checkpoint_path)
-			print(model.sample(sess, tokens, vocab, args.max_tokens, args.sample))
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+        (saved_args, reverse_input) = cPickle.load(f)
+    with open(os.path.join(args.save_dir, 'token_vocab.pkl'), 'rb') as f:
+        tokens, vocab = cPickle.load(f)
+    model = Model(saved_args, reverse_input, True)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver(tf.global_variables())
+        ckpt = tf.train.get_checkpoint_state(args.save_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print(model.sample(sess, tokens, vocab, args.max_tokens, args.sample))
 
 if __name__ == '__main__':
-	main()
+    main()
